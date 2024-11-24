d0cf65138085a41f994d1c34b4443065539d7ef9
yarik <yarsotnikoff@gmail.com>
2024-11-24 14:42:48 +0300

    первый коммит в мерджконфликт номер 2

--- RCC.py
+++ RCC.py
@@ -4,17 +4,17 @@ a = int(input())
 b = int(input())
 
 if a >= b:
-    print(b)
-else:
     print(a)
+else:
+    print(b)
 
-print("выводим разность квадратов:")
-print((a - b)*(a + b))
 print("выводим сумму квадратов:")
 print(a * a + b * b)
-print('вывод квадрата разности:')
-print((a - b)*(a - b))
+print("выводим разность квадратов:")
+print((a - b)*(a + b))
 print('выводим квадрат суммы:')
 print((a + b)*(a + b))
+print('вывод квадрата разности:')
+print((a - b)*(a - b))
 
 
